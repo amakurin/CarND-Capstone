b8ee0beec06855ef7f9311f2f5204edf6e7dc45f
perizen <perizen1@gmail.com>
2017-09-19 22:21:44 +0800

    Moved feature extraction functions outside of tl classifier class Other debugs

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -43,6 +43,103 @@ STATE_COUNT_THRESHOLD = 3
 
 MAX_DECEL = 1.
 
+#Functions for traffic light feature extraction (Traffic light state classification)
+# Define a function to return HOG features and visualization
+def get_hog_features(img, orient, pix_per_cell, cell_per_block,
+                     vis=False, feature_vec=True):
+    # Call with two outputs if vis==True
+    if vis == True:
+        features, hog_image = hog(img, orientations=orient,
+                                  pixels_per_cell=(pix_per_cell, pix_per_cell),
+                                  cells_per_block=(cell_per_block, cell_per_block),
+                                  transform_sqrt=True,
+                                  visualise=vis, feature_vector=feature_vec)
+        return features, hog_image
+    # Otherwise call with one output
+    else:
+        features = hog(img, orientations=orient,
+                       pixels_per_cell=(pix_per_cell, pix_per_cell),
+                       cells_per_block=(cell_per_block, cell_per_block),
+                       transform_sqrt=True,
+                       visualise=vis, feature_vector=feature_vec)
+        return features
+
+
+# Define a function to compute binned color features
+def bin_spatial(img, size=(32, 32)):
+    # Use cv2.resize().ravel() to create the feature vector
+    features = cv2.resize(img, size).ravel()
+    # Return the feature vector
+    return features
+
+
+# Define a function to compute color histogram features
+# NEED TO CHANGE bins_range if reading .png files with mpimg!
+def color_hist(img, nbins=32, bins_range=(0, 256)):
+    # Compute the histogram of the color channels separately
+    channel1_hist = np.histogram(img[:, :, 0], bins=nbins, range=bins_range)
+    channel2_hist = np.histogram(img[:, :, 1], bins=nbins, range=bins_range)
+    channel3_hist = np.histogram(img[:, :, 2], bins=nbins, range=bins_range)
+    # Concatenate the histograms into a single feature vector
+    hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
+    # Return the individual histograms, bin_centers and feature vector
+    return hist_features
+
+
+# Define a function to extract features from a list of images
+# Have this function call bin_spatial() and color_hist()
+def extract_features(img, color_space='RGB', spatial_size=(32, 32),
+                     hist_bins=32, orient=9,
+                     pix_per_cell=8, cell_per_block=2, hog_channel=0,
+                     spatial_feat=True, hist_feat=True, hog_feat=True, resize=False):
+    # Create a list to append feature vectors to
+    features = []
+    # Iterate through the list of images
+    file_features = []
+    # Read in each one by one
+    image = img
+    if resize == True:
+        image = cv2.resize(image, (15, 30))
+    # apply color conversion if other than 'RGB'
+    if color_space != 'RGB':
+        if color_space == 'HSV':
+            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+        elif color_space == 'LUV':
+            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
+        elif color_space == 'HLS':
+            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+        elif color_space == 'YUV':
+            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+        elif color_space == 'YCrCb':
+            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
+    else:
+        feature_image = np.copy(image)
+
+    if spatial_feat == True:
+        spatial_features = bin_spatial(feature_image, size=spatial_size)
+        file_features.append(spatial_features)
+    if hist_feat == True:
+        # Apply color_hist()
+        hist_features = color_hist(feature_image, nbins=hist_bins)
+        file_features.append(hist_features)
+    if hog_feat == True:
+        # Call get_hog_features() with vis=False, feature_vec=True
+        if hog_channel == 'ALL':
+            hog_features = []
+            for channel in range(feature_image.shape[2]):
+                hog_features.append(get_hog_features(feature_image[:, :, channel],
+                                                     orient, pix_per_cell, cell_per_block,
+                                                     vis=False, feature_vec=True))
+            hog_features = np.ravel(hog_features)
+        else:
+            hog_features = get_hog_features(feature_image[:, :, hog_channel], orient,
+                                            pix_per_cell, cell_per_block, vis=False, feature_vec=True)
+        # Append the new feature vector to the features list
+        file_features.append(hog_features)
+    features.append(np.concatenate(file_features))
+    # Return list of feature vectors
+    return features
+
 class TLDetector(object):
     def __init__(self):
         rospy.init_node('tl_detector')
@@ -199,99 +296,6 @@ class TLDetector(object):
 
         return (x, y)
 
-    # Define a function to return HOG features and visualization
-    def get_hog_features(img, orient, pix_per_cell, cell_per_block,
-                         vis=False, feature_vec=True):
-        # Call with two outputs if vis==True
-        if vis == True:
-            features, hog_image = hog(img, orientations=orient,
-                                      pixels_per_cell=(pix_per_cell, pix_per_cell),
-                                      cells_per_block=(cell_per_block, cell_per_block),
-                                      transform_sqrt=True,
-                                      visualise=vis, feature_vector=feature_vec)
-            return features, hog_image
-        # Otherwise call with one output
-        else:
-            features = hog(img, orientations=orient,
-                           pixels_per_cell=(pix_per_cell, pix_per_cell),
-                           cells_per_block=(cell_per_block, cell_per_block),
-                           transform_sqrt=True,
-                           visualise=vis, feature_vector=feature_vec)
-            return features
-
-    # Define a function to compute binned color features
-    def bin_spatial(img, size=(32, 32)):
-        # Use cv2.resize().ravel() to create the feature vector
-        features = cv2.resize(img, size).ravel()
-        # Return the feature vector
-        return features
-
-    # Define a function to compute color histogram features
-    # NEED TO CHANGE bins_range if reading .png files with mpimg!
-    def color_hist(img, nbins=32, bins_range=(0, 256)):
-        # Compute the histogram of the color channels separately
-        channel1_hist = np.histogram(img[:, :, 0], bins=nbins, range=bins_range)
-        channel2_hist = np.histogram(img[:, :, 1], bins=nbins, range=bins_range)
-        channel3_hist = np.histogram(img[:, :, 2], bins=nbins, range=bins_range)
-        # Concatenate the histograms into a single feature vector
-        hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
-        # Return the individual histograms, bin_centers and feature vector
-        return hist_features
-
-    # Define a function to extract features from a list of images
-    # Have this function call bin_spatial() and color_hist()
-    def extract_features(img, color_space='RGB', spatial_size=(32, 32),
-                         hist_bins=32, orient=9,
-                         pix_per_cell=8, cell_per_block=2, hog_channel=0,
-                         spatial_feat=True, hist_feat=True, hog_feat=True, resize=False):
-        # Create a list to append feature vectors to
-        features = []
-        # Iterate through the list of images
-        file_features = []
-        # Read in each one by one
-        image = img
-        if resize == True:
-            image = cv2.resize(image, (15, 30))
-        # apply color conversion if other than 'RGB'
-        if color_space != 'RGB':
-            if color_space == 'HSV':
-                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-            elif color_space == 'LUV':
-                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
-            elif color_space == 'HLS':
-                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
-            elif color_space == 'YUV':
-                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
-            elif color_space == 'YCrCb':
-                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
-        else:
-            feature_image = np.copy(image)
-
-        if spatial_feat == True:
-            spatial_features = bin_spatial(feature_image, size=spatial_size)
-            file_features.append(spatial_features)
-        if hist_feat == True:
-            # Apply color_hist()
-            hist_features = color_hist(feature_image, nbins=hist_bins)
-            file_features.append(hist_features)
-        if hog_feat == True:
-            # Call get_hog_features() with vis=False, feature_vec=True
-            if hog_channel == 'ALL':
-                hog_features = []
-                for channel in range(feature_image.shape[2]):
-                    hog_features.append(get_hog_features(feature_image[:, :, channel],
-                                                         orient, pix_per_cell, cell_per_block,
-                                                         vis=False, feature_vec=True))
-                hog_features = np.ravel(hog_features)
-            else:
-                hog_features = get_hog_features(feature_image[:, :, hog_channel], orient,
-                                                pix_per_cell, cell_per_block, vis=False, feature_vec=True)
-            # Append the new feature vector to the features list
-            file_features.append(hog_features)
-        features.append(np.concatenate(file_features))
-    # Return list of feature vectors
-    return features
-
     def get_light_state(self, light):
         """Determines the current color of the traffic light
 
@@ -354,14 +358,17 @@ class TLDetector(object):
                                                       hist_feat=hist_feat, hog_feat=hog_feat, resize=True)
             classifydata = np.vstack((classifylight_features)).astype(np.float64)
             predictedclass = clf.predict(classifydata)
-            if predictedclass == 1
+            if predictedclass == 1:
                 state = TrafficLight.YELLOW
+                print "Yellow Light"
                 continue
-            elif predictedclass == 2
+            elif predictedclass == 2:
                 state = TrafficLight.GREEN
+                print "Green light"
                 continue
-            elif predictedclass == 0
+            elif predictedclass == 0:
                 state = TrafficLight.RED
+                print "Red Light"
                 break  # Red has high priority, so, return it if it is seen
         #print(state)
         return state
